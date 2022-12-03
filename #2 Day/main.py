352b0f7b2369586dcb683ee54f59557da26adf10
Dhruvin Dhameliya <dhruvin82003@gmail.com>
2022-12-03 22:22:54 +0530

    Comments & Print Statement

---
+++ #2 Day/main.py
@@ -0,0 +1,21 @@
+# Todays learn about Comment & Print method!
+
+# This is single line comment in Python.
+print("Above single line comment, it's not print.")
+
+'''
+Author: @Dhruvin Dhameliya
+Course: #100daysOfCode
+'''
+print("Both the line is multi comment lines!")
+
+# -------------- Print statement --------------
+print("Hello World")  # Normal print statement
+
+print("Hey", "How are you?", 100)  # Multi values with print statement
+
+# sep is Separator (Defult separator is space(''))
+print("Hi", "Coders", 2.0, sep="~")
+
+# specify what to print at the end
+print("Hi", "Programmer", 2, end="100DaysOfCode")
